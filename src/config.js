714c291f1c4b1fd81185d800484f9506f4df6cd0
PethersonML <petherson.monteiro@hotmail.com>
2024-04-23 23:50:09 -0300

    Updating information for deployment on SaveinCloud

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "localhost",
-  user: process.env.DATABASE_USER || "fazt",
-  password: process.env.DATABASE_PASSWORD || "mypassword",
+  host: process.env.DATABASE_HOST || "node180656-brasiljs.jelastic.saveincloud.net",
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "YIDcga24313",
   database: process.env.DATABASE_NAME || "linksdb",
-  port: process.env.DATABASE_PORT || 3306,
+  port: process.env.DATABASE_PORT || 14520,
 };
 
 export const port = process.env.PORT || 4000;
