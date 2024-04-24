3c2c878f28ee60b7618fdd371e3e19b3d751e209
PethersonML <petherson.monteiro@hotmail.com>
2024-04-24 00:37:10 -0300

    Updating ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    apps : [{
-      name   : "app-presentation",
-      script : "npm start",
-      env_production: {
-         NODE_ENV: "production"
-      },
-      env_development: {
-         NODE_ENV: "development"
-      }
+    apps: [{
+        name: "app-presentation",
+        script: "node src/index.js",
+        env_production: {
+            NODE_ENV: "production"
+        },
+        env_development: {
+            NODE_ENV: "development"
+        }
     }]
-  }
+}
